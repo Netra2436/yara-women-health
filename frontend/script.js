12d5ce05f8637c7bed2fb3a17aab76b74da76eb6
Shriya Wakdevi Kuppa <154955651+shriyak-10@users.noreply.github.com>
2025-12-13 17:05:37 +0530

    Point frontend to Render backend

--- frontend/script.js
+++ frontend/script.js
@@ -6,11 +6,12 @@ document.getElementById("pcos-form").addEventListener("submit", async (e) => {
   Object.keys(data).forEach(k => data[k] = parseFloat(data[k]));
 
   try {
-    const res = await fetch("http://localhost:8000/predict", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data)
+    const res = await fetch("https://yara-backend-5yyp.onrender.com/predict", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
     });
+
 
     const out = await res.json();
     const message = out.prediction === 1
